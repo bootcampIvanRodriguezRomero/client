8f96051e749a2a31753bb7e7196b7cffef0ba070
Ivan Alexander Rodriguez Romero <irodriro@emeal.nttdata.com>
2023-07-09 20:15:16 -0500

    validate emails

--- src/main/java/com/bootcamp/client/infraestructure/rest/dto/Client.java
+++ src/main/java/com/bootcamp/client/infraestructure/rest/dto/Client.java
@@ -1,6 +1,7 @@
 package com.bootcamp.client.infraestructure.rest.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 @Data
@@ -15,6 +16,7 @@ public class Client {
     @NotBlank(message = "full name is required")
     private String fullName;
     @NotBlank(message = "email is required")
+    @Email
     private String email;
     @NotBlank(message = "type is required")
     private String type;
--- src/main/java/com/bootcamp/client/infraestructure/rest/dto/ClientPost.java
+++ src/main/java/com/bootcamp/client/infraestructure/rest/dto/ClientPost.java
@@ -1,6 +1,7 @@
 package com.bootcamp.client.infraestructure.rest.dto;
 import lombok.Data;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Email;
 @Data
 public class ClientPost {
     @NotBlank(message = "document type is required")
@@ -10,6 +11,7 @@ public class ClientPost {
     @NotBlank(message = "full name is required")
     private String fullName;
     @NotBlank(message = "email is required")
+    @Email
     private String email;
     @NotBlank(message = "type is required")
     private String type;
